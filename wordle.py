3bcf1945e09dae800e950e6e8dfa032317c6f05f
Nicholas Simafranca <simaf001@umn.edu>
2022-01-25 18:01:26 -0600

    wordle solution

--- wordle.py
+++ wordle.py
@@ -1,1 +1,15 @@
-print("hello World")
+import math
+import pandas
+import csv
+
+
+def all_possible_words():
+
+    with open("possiblewords(1), "r") as file:
+        words = []
+        for word in file:
+            word = word.isupper().strip()
+            if len(word) == 5 and word.isalpha():
+                words.append(word)
+
+        print(words)
